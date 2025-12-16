59d2afbef7741627f5e6749567dbaea54a0eaf6d
snehababar5706 <snehababar57@gmail.com>
2025-12-16 12:16:42 +0530

    Created pojo class

---
+++ StudentManagementApp/src/main/java/com/sma/app/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.sma.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController 
+{
+	@RequestMapping("/")
+  public String preLogin()
+  {
+	  return "login";
+  }
+
+	public String 
+}
--- StudentManagementApp/src/main/java/com/sma/app/model/Student.java
+++ StudentManagementApp/src/main/java/com/sma/app/model/Student.java
@@ -1,7 +1,81 @@
 package com.sma.app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Student
 {
-    private String id;
-    private String name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String studentId;
+    private String studentFullName;
+    private String studentEmail;
+    private int studentAge;
+    private String studentCollageName;
+    private String studentCourse;
+    private String batchNumber;
+    private String batchMode;
+    private double feesPaid;
+    
+    
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentFullName() {
+		return studentFullName;
+	}
+	public void setStudentFullName(String studentFullName) {
+		this.studentFullName = studentFullName;
+	}
+	public String getStudentEmail() {
+		return studentEmail;
+	}
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+	public int getStudentAge() {
+		return studentAge;
+	}
+	public void setStudentAge(int studentAge) {
+		this.studentAge = studentAge;
+	}
+	public String getStudentCollageName() {
+		return studentCollageName;
+	}
+	public void setStudentCollageName(String studentCollageName) {
+		this.studentCollageName = studentCollageName;
+	}
+	public String getStudentCourse() {
+		return studentCourse;
+	}
+	public void setStudentCourse(String studentCourse) {
+		this.studentCourse = studentCourse;
+	}
+	public String getBatchNumber() {
+		return batchNumber;
+	}
+	public void setBatchNumber(String batchNumber) {
+		this.batchNumber = batchNumber;
+	}
+	public String getBatchMode() {
+		return batchMode;
+	}
+	public void setBatchMode(String batchMode) {
+		this.batchMode = batchMode;
+	}
+	public double getFeesPaid() {
+		return feesPaid;
+	}
+	public void setFeesPaid(double feesPaid) {
+		this.feesPaid = feesPaid;
+	}
+    
+    
+    
 }
